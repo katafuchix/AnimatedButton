bc78f1029c7250496eb8702c0ec5bd842862901a
katafuchix <katafuchix@gmail.com>
2025-03-20 10:52:15 +0900

    Initial Commit

---
+++ AnimatedButton/AnimatedButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedButtonApp.swift
+//  AnimatedButton
+//
+//  Created by cano on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
